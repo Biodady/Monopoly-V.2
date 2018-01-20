843ef703f2b8766525bf1d805216ed0c9e419ace
Biodady <35559573+Biodady@users.noreply.github.com>
2018-01-21 00:10:22 +0700

    Initial commit

---
+++ Monopoly V.2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Monopoly_V._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbDis = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lbRoll = new System.Windows.Forms.Label();
+            this.lbCheck = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.lbDis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbDis
+            // 
+            this.lbDis.Image = ((System.Drawing.Image)(resources.GetObject("lbDis.Image")));
+            this.lbDis.Location = new System.Drawing.Point(12, 12);
+            this.lbDis.Name = "lbDis";
+            this.lbDis.Size = new System.Drawing.Size(758, 529);
+            this.lbDis.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lbDis.TabIndex = 0;
+            this.lbDis.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(642, 442);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(77, 74);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(331, 356);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(118, 55);
+            this.btnRoll.TabIndex = 2;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // lbRoll
+            // 
+            this.lbRoll.AutoSize = true;
+            this.lbRoll.Location = new System.Drawing.Point(369, 303);
+            this.lbRoll.Name = "lbRoll";
+            this.lbRoll.Size = new System.Drawing.Size(16, 17);
+            this.lbRoll.TabIndex = 3;
+            this.lbRoll.Text = "0";
+            // 
+            // lbCheck
+            // 
+            this.lbCheck.AutoSize = true;
+            this.lbCheck.Location = new System.Drawing.Point(202, 300);
+            this.lbCheck.Name = "lbCheck";
+            this.lbCheck.Size = new System.Drawing.Size(16, 17);
+            this.lbCheck.TabIndex = 4;
+            this.lbCheck.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbCheck);
+            this.Controls.Add(this.lbRoll);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.lbDis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lbDis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox lbDis;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Label lbRoll;
+        private System.Windows.Forms.Label lbCheck;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Monopoly V.2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monopoly_V._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int roll = 0;
+        int x1 = 45, x2 = 90, x3 = 135, x4 = 180, x5 = 225, x6 = 270; //270 //32
+        int y1 = 45, y2 = 90, y3 = 135, y4 = 180, y5 = 225, y6 = 270;
+        int currentDis = 0;
+        
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            roll = rnd.Next(1, 1);
+            lbRoll.Text = roll.ToString();
+            lbDis.Text = player.Top.ToString();
+            lbCheck.Text = player.Left.ToString();
+            if (player.Left >= 77)
+            {
+                if (roll == 1)
+                {
+                    player.Left -= x1;
+                }
+                else if (roll == 2)
+                {
+                    player.Left -= x2;
+                }
+                else if (roll == 3)
+                {
+                    player.Left -= x3;
+                }
+                else if (roll == 4)
+                {
+                    player.Left -= x4;
+                }
+                else if (roll == 5) 
+                {
+                    player.Left -= x5;
+                }
+                else if (roll == 6)
+                {
+                    player.Left -= x6;
+                    currentDis += roll;
+                }
+            }
+            else if(player.Left < 77)
+            {
+                if (roll == 1)
+                {
+                    player.Top -= y1;
+                }
+                else if (roll == 2)
+                {
+                    player.Top -= y2;
+                }
+                else if (roll == 3)
+                {
+                    player.Top -= y3;
+                }
+                else if (roll == 4)
+                {
+                    player.Top -= y4;
+                }
+                else if (roll == 5)
+                {
+                    player.Top -= y5;
+                }
+                else if (roll == 6)
+                {
+                    player.Top -= y6;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
